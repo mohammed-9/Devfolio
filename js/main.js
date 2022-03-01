c6b6a236f6eb8b88882d96beef1b4af440b49c07
mohammed-9 <adnan.sy.no@gmail.com>
2022-03-01 06:02:31 +0200

    responsive project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let typed = new Typed('#anim', {
+    // Waits 1000ms after typing "First"
+    strings: ['Photographer','Designer','Deveolper','Freelance'],
+loop: true,
+typeSpeed: 300,
+backSpeed: 100,
+backDelay: 50,
+  });
+
+  function test()
+  {
+    let x= 10;
+    console.log(x);
+  }
+  test();
+ 
